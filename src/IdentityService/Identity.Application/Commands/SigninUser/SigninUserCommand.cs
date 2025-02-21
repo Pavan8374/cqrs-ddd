d8eeba25f549ed480dacb15cae38a5f04f32ca91
Pavan8374 <rpavank2002@gmail.com>
2025-02-21 18:59:44 +0530

    implement login

---
+++ src/IdentityService/Identity.Application/Commands/SigninUser/SigninUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using Identity.Application.DTOs;
+using Identity.Domain.Common;
+using MediatR;
+
+namespace Identity.Application.Commands.SigninUser
+{
+    public class SigninUserCommand : IRequest<Result<AuthResponseDto>>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
